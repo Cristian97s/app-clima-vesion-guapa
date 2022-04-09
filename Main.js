58edf1488c8d552ce45c9ca6fae90188744c66af
Cristian97s <cristianaleman1997@gmail.com>
2022-04-08 20:00:40 -0600

    primera version

---
+++ Main.js
@@ -0,0 +1,77 @@
+
+const APP_ID = '4090239d69cdb3874de692fd18539299';
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+const themeToggler = document.querySelector(".theme-toggler");
+
+menuBtn.addEventListener('click', () =>{
+    sideMenu.style.display ='block';
+})
+
+closeBtn.addEventListener('click', () =>{
+    sideMenu.style.display ='none';
+})
+
+themeToggler.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark-theme-variable');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+Orders.forEach(order =>{
+    const tr = document.createElement('tr');
+    const trContent = `<td>${order.productName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'pending' ? 'warning' : 'primary'}">${order.shipping}</td>
+                        <td class="primary">Details</td>`;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+})
+
+const fetchData = position => {
+    const { latitude, longitude } = position.coords;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${APP_ID}`)
+        .then(response => response.json())
+        .then(data => setWeatherData(data));
+}
+
+const setWeatherData = data => {
+    const weatherData = {
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: Math.floor(data.main.temp),
+        date: getDate(),
+    }
+
+    Object.keys(weatherData).forEach( key => {
+        setTextContent(key, weatherData[key]);
+    });
+
+    cleanUp();
+}
+
+const cleanUp = () => {
+    let container = document.getElementById('container');
+
+    loader.style.display = 'none'; 
+    container.style.display = 'flex'; 
+}
+
+const getDate = () => {
+    let date = new Date();
+    return `${date.getDate()}-${ ('0' + (date.getMonth() + 1)).slice(-2)}-${date.getFullYear()}`;
+}
+
+const setTextContent = (element, text) => {
+    document.getElementById(element).textContent = text;
+}
+
+const onLoad = () => {
+    navigator.geolocation.getCurrentPosition(fetchData)
+}
